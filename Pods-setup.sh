7ca78b01380e3b85a420e19e2eb721f39f0f9934
Robert Hannah <rhannah@mitre.org>
2014-08-04 12:09:57 -0400

    l

--- Pods-setup.sh
+++ Pods-setup.sh
@@ -6,33 +6,37 @@ TAG="[Pods-setup.sh]:"
 ALL=0
 CONFIG=0
 MAKE=0
-INSTALL=0
 UPDATE=0
+NONE=0
 
 for arg in "$@"; do
 	case $arg in
 		-a|--all) ALL=1 ;;
 		-c|--config) CONFIG=1 ;;
 		-m|--make) MAKE=1 ;;
-		-i|--install) INSTALL=1 ;;
 		-U|--update) UPDATE=1 ;;
+		-n|--none) NONE=1 ;;
 		*);;
 	esac
 done
 
-if [ $ALL -eq 0 && $CONFIG -eq 0 && $MAKE -eq 0 && $INSTALL -eq 0 && $UPDATE -eq 0 ]; then
-	echo "usage: ./Pods-setup.sh [-a | -c | -m | -i | -U]"
-	echo "	At least one flag is required"
-	echo ""
-	echo "	-a, --all		Attempt to automate all necessary changes, including"
-	echo "					compile and install multipiler and clang."
-	echo "	-c, --config	... Stop before calling './configure'"
-	echo "	-m, --make 		... Stop before calling 'make'"
-	echo "	-i, -install 	... Stop before calling 'make install'"
-	echo "	-U, --update 	Ignore 'tools/clang' contents and force overwite with"
-	echo "					original 'multicompiler-clang' directory"
-	echo ""
-	exit 0
+if [ $ALL -eq 0 -a $CONFIG -eq 0 ]; then
+	if [ $MAKE -eq 0 -a $NONE -eq 0 ]; then
+		echo "usage: ./Pods-setup.sh [-a | -c | -m | -i | -U]"
+		echo ""
+		echo "	At least one [acmn] flag is required"
+		echo ""
+		echo "	-a, --all		Attempt to automate all necessary changes, including"
+		echo "				compile and install multipiler and clang."
+		echo "	-c, --config 		configure only"
+		echo "	-m, --make		configure and make"
+		echo "	-n, --none		bare minimum directory changes only"
+		echo ""
+		echo "	-U, --update 		Ignore 'tools/clang' contents and force overwite with"
+		echo "				original 'multicompiler-clang' directory"
+		echo ""
+		exit 0
+	fi
 fi
 
 # Setup directory variables
@@ -45,25 +49,30 @@ PODSPEC="$TOOLS_DIR/multicompiler-clang.podspec"
 
 # Directory configuration
 if [ -d $TOOLS_DIR ]; then
+	echo "$TAG '$TOOLS_DIR' exists..."
 	if [ -f $PODSPEC ]; then
 		if [ $UPDATE -eq 0 ]; then
-			echo "$TAG '$TOOLS_DIR' ready..."
+			echo "$TAG '$TOOLS_DIR' ready!"
+			echo ""
 		else
 			echo "$TAG Updating tools/clang..."
 			echo "rm -rf $TOOLS_DIR"
 			rm -rf $TOOLS_DIR
-			echo "mv $CLANG_DIR $TOOLS_DIR"
-			mv $CLANG_DIR $TOOLS_DIR
+			echo "cp $CLANG_DIR $TOOLS_DIR"
+			cp -r $CLANG_DIR $TOOLS_DIR
+			echo ""
 		fi
 	else
 		# tools/clang is incomplete. attempt to copy
+		echo "$TAG clang directory incomplete. Attempting to move original."
 		if [ -d $CLANG_DIR ]; then 
 			# Move clang into tools directory
 			echo "$TAG Moving '$CLANG_DIR' to '$TOOLS_DIR'..."
 			echo "rm -rf $TOOLS_DIR"
 			rm -rf $TOOLS_DIR
-			echo "mv $CLANG_DIR $TOOLS_DIR"
-			mv $CLANG_DIR $TOOLS_DIR
+			echo "cp -r $CLANG_DIR $TOOLS_DIR"
+			cp -r $CLANG_DIR $TOOLS_DIR
+			echo ""
 		else
 			echo "$TAG '$CLANG_DIR' directory does not exist."
 			echo "$TAG Please run 'pod install' from your app's top directory."
@@ -73,9 +82,10 @@ if [ -d $TOOLS_DIR ]; then
 	fi
 else
 	if [ -d $CLANG_DIR ]; then
-		echo "$TAG Moving '$CLANG_DIR' to '$TOOLS_DIR'..."
-		echo "mv $CLANG_DIR $TOOLS_DIR"
-		mv $CLANG_DIR $TOOLS_DIR
+		echo "$TAG Copying '$CLANG_DIR' to '$TOOLS_DIR'..."
+		echo "cp -r $CLANG_DIR $TOOLS_DIR"
+		cp -r $CLANG_DIR $TOOLS_DIR
+		echo ""
 	else
 		echo "$TAG '$CLANG_DIR' directory does not exist."
 		echo "$TAG Please run 'pod install' from your app's top directory."
@@ -89,10 +99,12 @@ if [ ! -d $BUILD_DIR ]; then
 	echo "$TAG Creating '$BUILD_DIR' prefix for clang installation..."
 	echo "mkdir $BUILD_DIR"
 	mkdir $BUILD_DIR
+else
+	echo "$TAG build directory already exists..."
 fi
 
-if [ $CONFIG -eq 0 ]; then
-	echo "$TAG -c (--config) flag not given: stopping..."
+if [ $NONE -eq 1 ]; then
+	echo "$TAG -n (--none) flag given: stopping before configure..."
 	echo "$TAG Please './configure [flags]', 'make', and 'make install' the multicompiler."
 	echo "$TAG Exiting..."
 	exit 1
@@ -112,16 +124,16 @@ else
 	exit -1
 fi
 
-if [ $MAKE -eq 0 ]; then
-	echo "$TAG -m (--make) flag not given: stopping..."
+if [ $CONFIG -eq 1 ]; then
+	echo "$TAG -c (--config) flag given: stopping before make..."
 	echo "$TAG Please 'make' and 'make install' the multicompiler."
 	echo "$TAG Exiting..."
 	exit 2
 fi
 
 echo "$TAG Starting make of multicompiler... (this may take some time)"
-echo "make -j 3"
-make -j 3
+echo "make"
+make
 RET=$?
 
 if [ $RET -eq 0 ]; then
@@ -132,8 +144,8 @@ else
 	exit -1
 fi
 
-if [ $INSTALL -eq 0 ]; then
-	echo "$TAG -i (--install) flag not given: stopping..."
+if [ $MAKE -eq 1 ]; then
+	echo "$TAG -m (--make) flag given: stopping before install..."
 	echo "$TAG Please 'make install' to install into $BUILD_DIR."
 	echo "$TAG Exiting..."
 	exit 3
